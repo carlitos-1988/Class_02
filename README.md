d231b33a82aa0da4a365fce905e9fe6781edccad
Juan C Olmedo <doubleparked88@gmail.com>
2023-03-08 18:09:35 -0800

    Done for the day

--- README.md
+++ README.md
@@ -6,6 +6,7 @@
 
 I built this site so that you can get to know me better (not much). The purpose is to provide a quick intro of myself and ask questions based on what you have read.
 
+
 ### Author: Juan Olmedo
 
 ### Links and Resources
@@ -15,7 +16,12 @@ I built this site so that you can get to know me better (not much). The purpose 
 * Provide a screenshot of your score after running a Lighthouse Accessibility report.
 image.png
 
+image.png - Lighthouse Score 85;
+
 ### Reflections and Comments
 
 Great practice with git, command line, and github for project initialization. Also the use srch files for the project. 
+
+8Mar2023
+a lot of interesting things for this assignment had a hard time counting the mumber of loops in my head. Also, struggeled with what is in scope vs out of scope for the loops. Lastly I forgot that == will check against the variable from string to a primitive data type. I know this should have worked but for some reason it did not work for me. 
 
--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 'use strict'
 
+let totalScore = 0;
+
     let welcomeUser = prompt("Hello What is your name?");
-    alert("Welcome " + welcomeUser + " I'm glad you have come to my site")
+    alert("Welcome " + welcomeUser + " I'm glad you have come to my cite")
 
     let home = prompt("do I currently reside in Washington?").toLowerCase();
     if(home === "true"){
         //console.log("That is correct I live in Washington");
+        totalScore +=1
         alert("That is correct I live in Washington")
     }else{
         //console.log("That is not correct I live in Washington");
@@ -15,6 +18,7 @@
     let job = prompt("Was I in the Air Force?").toLowerCase();
     if(job === 'true'){
         //console.log("That is correct I was in the Air Force");
+        totalScore +=1;
         alert("That is correct I was in the Air Force");
     }else{
         //console.log("Incorrect I was in the Air Force");
@@ -24,6 +28,7 @@
     let certification = prompt("Do I have any certifications?").toLowerCase();
     if(certification === 'true'){
         //console.log("Correct I have a sec+");
+        totalScore +=1;
         alert("Correct I have a sec+");
     }else{
        // console.log("Incorrect I have a sec+")
@@ -35,13 +40,15 @@
         //console.log("Incorrect I have not worked at applebees");
         alert("Incorrect I have not worked at applebees");
     }else{
-        console.log("Correct I have not worked at Applebees");
+        //console.log("Correct I have not worked at Applebees");
+        totalScore +=1;
         alert("Correct I have not worked at Applebees");
     }
     
     let goal = prompt("Do I want to work on the cloud?").toLowerCase();
     if(goal === "true"){
        // console.log("Correct I want to work in the cloud");
+       totalScore +=1;
        alert("Correct I want to work in the cloud");
     }else{
         console.log("Incorrect I do want to work in the cloud");
@@ -49,7 +56,71 @@
     }
 
 
+// while(totalAttempts<=4){
+//     if(guessedNumber === myAnser){
+//         alert("That is Correct that was my number you did it in " + (4-totalAttempts) + " attempts" );
+//         break;
+//     }else if(guessedNumber>myAnser && totalAttempts<= 4){
+//         totalAttempts++;
+//         alert("number whas too high, You have" + (4-totalAttempts) + " left");
+//     }else if(guessedNumber<myAnser && totalAttempts<=4){
+//         totalAttempts++;
+//         alert("number was too low, you have " + (4-totalAttempts) + "left")
+//     }else{
+//         console.log("Something went wrong");
+//     }
+// }
+
+let guessedNumber =  prompt("please guess a number between 1-10 you will have a total of 4 guesses");
+let myAnser = 8;//I know it's spelled answer
+let totalAttempts = 0;
+// let maxAttempts = 3;
+
+for(let i = 0;i<=2; i++){
    
+    if(guessedNumber == myAnser){
+        totalAttempts++;
+        alert("That is Correct you did it in "+ totalAttempts + " attempts");
+        console.log(typeof guessedNumber);
+        totalScore +=1;
+        break;
+    }else if(guessedNumber>myAnser){
+        totalAttempts++;
+        alert("number whas too high, Attempt# " + totalAttempts);
+        guessedNumber =  prompt("please guess a number between 1-10");
+        //console.log(typeof guessedNumber);
+    }else if(guessedNumber<myAnser){
+        totalAttempts++;
+        alert("number was too low, Attempt# " + totalAttempts);
+        guessedNumber =  prompt("please guess a number between 1-10");
+        //console.log(typeof guessedNumber);
+    }else if(totalAttempts == '4'){
+        alert("maximum number of entries exausted");
+        break;
+    }
+}
+   
+
+let possibleGuesses = 0;
+let answers = ["dragonBall z","pokemon","shamanKing", "demon slayer", "one-punch man","naruto"];
+
+let question7 = 0;
+while(question7<7){
+    question7+=1;
+    console.log(answers[5]);
+    alert("Choose from the following answers" + answers);
+    let anime = prompt("Question 7: Please tell me what is my current Anime?").toLocaleLowerCase();
+    if(anime == answers[5]){
+        alert("that is correct");
+        totalScore +=1;
+        break
+    }else{
+        question7+=1;
+        alert("that was not correct");
+    }
+}
+
+alert("Your total score is " + totalScore );
 
 
     
